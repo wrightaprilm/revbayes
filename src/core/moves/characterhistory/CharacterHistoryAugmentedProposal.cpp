b4d65cca0c4134152981ab9b6414e792add7b796
shiranab <shiranabadi@mail.tau.ac.il>
2020-02-10 12:05:03 -0800

    create new DA move files

---
+++ src/core/moves/characterhistory/CharacterHistoryAugmentedProposal.cpp
@@ -0,0 +1,134 @@
+#include "CharacterHistoryAugmentedProposal.h"
+
+#include <stddef.h>
+#include <vector>
+
+#include "RbException.h"
+#include "StateDependentSpeciationExtinctionProcess.h"
+#include "Tree.h"
+#include "Cloneable.h"
+#include "StochasticNode.h"
+#include "TypedDistribution.h"
+
+namespace RevBayesCore { class DagNode; }
+
+using namespace RevBayesCore;
+
+/**
+ * Constructor
+ *
+ * Here we simply allocate and initialize the Proposal object.
+ */
+CharacterHistoryAugmentedProposal::CharacterHistoryAugmentedProposal( StochasticNode<Tree> *n ) : Proposal(),
+    variable( n )
+{
+    // tell the base class to add the node
+    addNode( variable );
+    
+    distribution = dynamic_cast< StateDependentSpeciationExtinctionProcess* >( &variable->getDistribution() );
+    if ( distribution == NULL )
+    {
+        throw RbException("The CharacterHistoryAugmentedProposal is currently only implemented for CDBDP distributions.");
+    }
+}
+
+
+/**
+ * The cleanProposal function may be called to clean up memory allocations after AbstractMove
+ * decides whether to accept, reject, etc. the proposed value.
+ *
+ */
+void CharacterHistoryAugmentedProposal::cleanProposal( void )
+{
+    
+    
+}
+
+
+CharacterHistoryAugmentedProposal* CharacterHistoryAugmentedProposal::clone( void ) const
+{
+
+        return new CharacterHistoryAugmentedProposal( *this );
+}
+
+/**
+ * Perform the proposal.
+ *
+ * \return The hastings ratio.
+ */
+double CharacterHistoryAugmentedProposal::doProposal( void )
+{
+
+    size_t num_nodes = distribution->getValue().getNumberOfNodes();
+    std::vector<std::string> character_histories(num_nodes);
+    distribution->drawStochasticCharacterMap(character_histories);
+    
+    return 0.0;
+}
+
+
+const std::string& CharacterHistoryAugmentedProposal::getProposalName( void ) const
+{
+    static std::string name = "CharacterHistoryAugmentedProposal";
+    return name;
+}
+
+
+double CharacterHistoryAugmentedProposal::getProposalTuningParameter( void ) const
+{
+    // this proposal has no tuning parameter
+    return RbConstants::Double::nan;
+}
+
+
+void CharacterHistoryAugmentedProposal::prepareProposal( void )
+{
+    
+}
+
+
+void CharacterHistoryAugmentedProposal::printParameterSummary(std::ostream &o, bool name_only) const
+{
+    
+    
+}
+
+
+void CharacterHistoryAugmentedProposal::undoProposal( void )
+{
+    
+    
+}
+
+
+/**
+ * Swap the current variable for a new one.
+ *
+ * \param[in]     oldN     The old variable that needs to be replaced.
+ * \param[in]     newN     The new RevVariable.
+ */
+void CharacterHistoryAugmentedProposal::swapNodeInternal(DagNode *oldN, DagNode *newN)
+{
+    
+    variable = static_cast< StochasticNode<Tree>* >(newN) ;
+    
+    distribution = dynamic_cast< StateDependentSpeciationExtinctionProcess* >( &variable->getDistribution() );
+    if ( distribution == NULL )
+    {
+        throw RbException("The CharacterHistoryAugmentedProposal is currently only implemented for CDBDP distributions.");
+    }
+}
+
+
+void CharacterHistoryAugmentedProposal::setProposalTuningParameter(double tp)
+{
+    // this proposal has no tuning parameter: nothing to do
+}
+
+
+void CharacterHistoryAugmentedProposal::tune( double rate )
+{
+    
+    
+}
+
---
+++ src/core/moves/characterhistory/CharacterHistoryAugmentedProposal.h
@@ -0,0 +1,46 @@
+#ifndef CharacterHistoryAugmentedProposal_H
+#define CharacterHistoryAugmentedProposal_H
+
+#include <iosfwd>
+
+#include "Proposal.h"
+
+namespace RevBayesCore {
+class DagNode;
+class StateDependentSpeciationExtinctionProcess;
+class Tree;
+template <class variableType> class StochasticNode;
+    
+    class CharacterHistoryAugmentedProposal : public Proposal {
+        
+    public:
+        
+        CharacterHistoryAugmentedProposal(StochasticNode<Tree> *n);                                                                              //!<  constructor
+        void                                            cleanProposal(void);                                                                //!< Clean up proposal
+        CharacterHistoryAugmentedProposal*              clone(void) const;                                                                  //!< Clone object
+        double                                          doProposal(void);                                                                   //!< Perform proposal
+        const std::string&                              getProposalName(void) const;                                                        //!< Get the name of the proposal for summary printing
+        double                                          getProposalTuningParameter(void) const;
+
+        void                                            prepareProposal(void);                                                              //!< Prepare the proposal
+        void                                            printParameterSummary(std::ostream &o, bool name_only) const;                                       //!< Print the parameter summary
+        void                                            setProposalTuningParameter(double tp);
+        void                                            tune(double r);                                                                     //!< Tune the proposal to achieve a better acceptance/rejection ratio
+        void                                            undoProposal(void);                                                                 //!< Reject the proposal
+        
+    protected:
+
+        void                                            swapNodeInternal(DagNode *oldN, DagNode *newN);                                     //!< Swap the DAG nodes on which the Proposal is working on
+        
+        StateDependentSpeciationExtinctionProcess*      distribution;
+
+    private:
+        
+        StochasticNode<Tree>*                           variable;                                                                           //!< The variable the Proposal is working on
+        
+    };
+    
+}
+
+#endif
+
